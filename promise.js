96ea00bf726202a1419af1fb38439dd5bd5037f7
avilavate <avilavate123@gmail.com>
2017-05-12 10:20:12 +0530

    promise poc

---
+++ promise.js
@@ -0,0 +1,45 @@
+function a() {
+    var p = new Promise((res, rej) => {
+        setTimeout(function () {
+            rej("from a!");
+        }, 0);
+    });
+    return p;
+}
+
+function b() {
+    console.log("b called");
+    var p = new Promise((res, rej) => {
+        setTimeout(function () {
+            res("from b!");
+        }, 1000);
+    });
+    return p;
+}
+
+function c() {
+    var p = new Promise((res, rej) => {
+        setTimeout(function () {
+            res("from c!");
+        }, 3000);
+    });
+    return p;
+}
+
+a().
+    then(aMessage => {
+        console.log(aMessage);
+        return b();
+    }, errA=>{console.log("brom err fun of a", errA)}).
+
+    then(bMessage => {
+        console.log("b", bMessage);
+        return c();
+    })
+
+    .then(cMessage => {
+        console.log("c", cMessage);
+    })
+    .catch(reason => {
+        console.log("Catch block",reason);
+    })
